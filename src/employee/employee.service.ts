743579398cde1c12f4ed0964a5298b928eb6584e
LalitKhu <lalitkhudhania2@gmail.com>
2026-05-27 16:34:24 +0530

    feat: add find all employe service functionality

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -4,15 +4,33 @@ import { CreateEmployeeDto } from './dto/create-employee.dto';
 
 @Injectable()
 export class EmployeeService {
-  constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly prisma: PrismaService) { }
 
-  async findAll(page: number = 1, limit: number = 20, filters?: { name?: string; email?: string; jobTitle?: string; country?: string; department?: string; }) {
-   
-    return [];
-  }
+    async findAll(page: number = 1, limit: number = 20, filters?: { name?: string; email?: string; jobTitle?: string; country?: string; department?: string; }) {
+        const skip = (page - 1) * limit;
+        const where: any = {};
+        if (filters) {
+            if (filters.name) {
+                where.fullName = { contains: filters.name };
+            }
+            if (filters.email) {
+                where.email = { contains: filters.email };
+            }
+            if (filters.jobTitle) {
+                where.jobTitle = { contains: filters.jobTitle };
+            }
+            if (filters.country) {
+                where.country = { equals: filters.country };
+            }
+            if (filters.department) {
+                where.department = { equals: filters.department };
+            }
+        }
+        return this.prisma.employee.findMany({ skip, take: limit, where });
+    }
 
     async create(dto: CreateEmployeeDto) {
-        
+
         return {}
     }
 }
