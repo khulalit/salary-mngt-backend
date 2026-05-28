4c711954b6990052f99e8c05a9f067086ba2f59f
LalitKhu <lalitkhudhania2@gmail.com>
2026-05-28 13:05:07 +0530

    fix: removed validation pipe

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 
@@ -23,13 +22,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
 
   app.setGlobalPrefix('api');
 
